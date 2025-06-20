651ebac0dd672f104f20e3313fc540ea680b002a
Nikit0s21 <100759496+Nikit0s21@users.noreply.github.com>
2025-06-20 04:11:00 +0300

    Update settings.py

--- tutoring_system/settings.py
+++ tutoring_system/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tutoring_db',
         'USER': 'postgres',
-        'PASSWORD': 'oquojdar21',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
